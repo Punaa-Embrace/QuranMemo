89cc49c34b71f4f5fa1bbb6f7a7e121300c0477c
Punaa-Embrace <putrabtm055@gmail.com>
2026-05-17 09:32:33 +0700

    Memasukan Onboarding diawal Aplikasi Dibuka

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'page/main_page.dart';
 import 'theme/app_theme.dart';
+import '../page/auth/onboarding_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'QuranMemo',
       theme: AppTheme.lightTheme,
       debugShowCheckedModeBanner: false,
-      home: const MainPage(),
+      home: const OnboardingPage(),
     );
   }
 }
